a58568109dd11c3ab5413bdc5acbb2f5bb9a05e4
Riaan Swanepoel <riaan.swanepoel@fabi.up.ac.za>
2015-03-11 10:16:17 +0200

    Added seq_utils.py and count_all_seqs.py. Also modified env.sh to work on own computer

--- bin/count_all_seqs.py
+++ bin/count_all_seqs.py
@@ -1,1 +1,24 @@
-# An empty file
+#!/usr/bin/env python
+
+import sys
+import seq_utils
+#import glob
+import fnmatch
+import os
+
+if len(sys.argv) != 2:
+	print "Please input exactly 2 agruments"
+
+def count_all_seqs(filename):
+	input_file = open(filename)
+	seq_count = seq_utils.count_seqs(input_file)
+	print seq_count,"in", os.path.basename(filename)
+
+#for filename in glob.iglob(sys.argv[1] + '*.fasta'):
+#    count_all_seqs(filename)
+
+for filename in sorted(os.listdir(sys.argv[1])):
+	path = os.path.join(sys.argv[1], filename)
+	if fnmatch.fnmatch(path, '*.fasta'):
+		count_all_seqs(path)
+
